f75dd4bf4547b99e3f07aa0ac47f5b768c696c81
Jônatas T. Andrade <87863493+jonat4s@users.noreply.github.com>
2024-03-26 23:59:37 -0300

    Ehigie Method
    This method is used to calculate Vclay (or Vshale) using an NMR log. It is common if you desire to do this in carbonate reservoirs.

---
+++ Petrophysics/Ehigie_Method.py
@@ -0,0 +1,27 @@
+def vclay_ehigie(phit: npt.ArrayLike, phie: npt.ArrayLike):
+    """Estimate the clay volume (or shale volume) from the Ehigie model.
+    Parameters
+    ----------
+    cbw : array_like
+        Clay bound water.
+    phit : int, float
+        Porosity total.
+    phie : int, float
+        Porosity effective.
+         
+    Returns
+    -------
+    vshale : array_like
+        Shale Volume for the aimed interval using the Ehigie method.
+
+    References
+    ----------
+
+    Ehigie (2010)
+    """
+
+    cbw = phit - phie
+    vshale = cbw / phit
+
+
+    return vclay
